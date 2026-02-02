d91ec9e22f5dd3abbcee5f2a33565108befb6e7a
chandrashekarmurthy <svchandrashekarmurthy@gmail.com>
2026-02-02 16:03:08 +0530

    Fix string formatting in getName method

--- src/main/java/com/rt/sample/HelloWorld.java
+++ src/main/java/com/rt/sample/HelloWorld.java
@@ -29,7 +29,7 @@ public class HelloWorld {
 
 
 	public static String getName() {
-		String name = "Rushi Technologies Pvt Ltd ";
+		String name = "Rushi Technologies Pvt Ltd";
 		return name;
 	}
 
